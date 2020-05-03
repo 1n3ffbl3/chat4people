1446f4490b91daacf46ae828b65f65f423090a20
1n3ffbl3 <mizgier.m@gmail.com>
2020-05-03 16:45:10 +0200

    Template components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import UserBox from '../src/components/UserBox'
 
 function App() {
   return (
     <>
     <h1>chat4people</h1>
+    <UserBox/>
     </>
   );
 }
---
+++ frontend/src/components/UserBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserBox = () => (
+    <>
+        <div>
+            <img src='../img/rabbitTime' alt='userImg'></img>
+        </div>
+        <div>
+            <h2>User name</h2>
+            <p>last message...</p>
+        </div>
+    </>
+);
+
+export default UserBox;
