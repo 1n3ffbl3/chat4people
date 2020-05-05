d354a2bf072020cc110d1d9fb114d73df8feb40e
1n3ffbl3 <mizgier.m@gmail.com>
2020-05-05 19:03:55 +0200

    Separate chat from input message

--- frontend/src/components/ChatBox/ChatBox.js
+++ frontend/src/components/ChatBox/ChatBox.js
@@ -3,7 +3,14 @@ import styles from './ChatBox.module.scss';
 
 const ChatBox = () => (
   <>
-    <div className={styles.chatBoxContainer}></div>
+    <div className={styles.chatContainer}>
+      <div className={styles.chatBox}>
+        chatBox
+      </div>
+      <div className={styles.messageBox}>
+        <input className={styles.msgInput} placeholder="Type your message..." />
+      </div>
+    </div>
   </>
 );
 
