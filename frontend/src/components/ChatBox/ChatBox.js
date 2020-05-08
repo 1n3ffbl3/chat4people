8c4b60daceb99a7392ff7340fd34aa801f0ed23d
1n3ffbl3 <mizgier.m@gmail.com>
2020-05-08 18:29:37 +0200

    Start to add logic to display message in chat box WIP

--- frontend/src/components/ChatBox/ChatBox.js
+++ frontend/src/components/ChatBox/ChatBox.js
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './ChatBox.module.scss';
 import MessageBox from '../MessageBox/MessageBox.js';
 
-const ChatBox = () => (
-  <>
-    <div className={styles.chatContainer}>
-      <div className={styles.chatBox}>
-        <div className={styles.messages}>
-          <div className={styles.messagesUser1}>
-            test1
+
+const mockedMessages = [
+  {
+    id: 1,
+    message: 'test 1 ',
+  },
+  {
+    id: 2,
+    message: 'test 2 ',
+  },
+  {
+    id: 2,
+    message: 'test 3 ',
+  },
+  {
+    id: 1,
+    message: 'test 4 ',
+  },
+];
+
+
+const ChatBox = () => {
+  const [messages, setMessages] = useState(mockedMessages);
+
+  const msgStyle = id => 1 === id ?  styles.messagesUser1 : styles.messagesUser2;
+  const addMsg = txt => setMessages([...messages, {id: 2, message: txt}]);
+
+  return(
+    <>
+      <div className={styles.chatContainer}>
+        <div className={styles.chatBox}>
+          <div className={styles.messages}>
+            { messages.map(msg => (
+              <div className={msgStyle(msg.id)}>
+                {msg.message}
+              </div>
+            ))}
           </div>
-          <div className={styles.messagesUser2}>
-            test2
-          </div>
-  
         </div>
+        { /* TODO: Add better prop name to increase readibility */ }
+        <MessageBox sendMessage={addMsg}/> 
       </div>
-      <MessageBox />
-    </div>
-  </>
-);
+    </>
+  );
+};
+
+// TODO: add propTypes
 
 export default ChatBox;
--- frontend/src/components/MessageBox/MessageBox.js
+++ frontend/src/components/MessageBox/MessageBox.js
@@ -3,11 +3,14 @@ import React from 'react';
 import styles from './MessageBox.module.scss';
 
 
-const MessageBox = () => (
+const MessageBox = ({ sendMessage }) => (
   <div className={styles.messageBox}>
+    { /* TODO: Read value from contentEditable div and pass to sendMessage */ }
     <div className={styles.msgType} contentEditable="true"></div>
-    <button className={styles.btnSendMsg}>>></button>
+    <button className={styles.btnSendMsg} onClick={() => sendMessage('TEST')}>>></button>
   </div> 
 );
+
+// TODO: add propTypes
 
 export default MessageBox;
